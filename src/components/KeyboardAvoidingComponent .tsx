4298722055f878a77a410478cb52950ed965745c
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-18 19:03:46 +0600

    Keyboard Avoiding wrapper

---
+++ src/components/KeyboardAvoidingComponent .tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
+import tw from 'twrnc';
+
+type Props = {
+    children: React.ReactNode;
+};
+
+const KeyboardAvoidingComponent = ({ children }: Props) => {
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={tw`flex-1`}
+        >
+            <View style={tw`flex-1`}>
+                {children}
+            </View>
+        </KeyboardAvoidingView>
+    );
+};
+
+export default KeyboardAvoidingComponent;
