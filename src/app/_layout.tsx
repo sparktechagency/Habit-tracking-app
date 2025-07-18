6f4b8d9035488f7c50fba3d6d529d5941f0a92de
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-18 19:06:47 +0600

    Root Layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,41 @@
 import { Stack } from "expo-router";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  const { top, bottom } = useSafeAreaInsets();
-
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="index" options={{}} />
-      {/* Add other screens here */}
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          headerShown: false, // Hides header for all screens
+          animation: 'fade', // Smooth transition animation
+        }}
+      >
+        {/* Initial screens */}
+        <Stack.Screen name="index" />
+        <Stack.Screen name="+not-found" />
+
+        {/* Grouped screens */}
+        <Stack.Screen
+          name="(splash-screen)"
+          options={{
+            animation: 'slide_from_right',
+            gestureEnabled: false // Disable swipe back on splash screens
+          }}
+        />
+        <Stack.Screen
+          name='(auth)'
+          options={{
+            animation: 'slide_from_right',
+            gestureEnabled: false // Disable swipe back on splash screens
+          }}
+        />
+
+
+        {/* Add more groups as needed */}
+        {/* <Stack.Screen name="(app)" /> */}
+        {/* <Stack.Screen name="(onboarding)" /> */}
+      </Stack>
+    </>
   );
 }
