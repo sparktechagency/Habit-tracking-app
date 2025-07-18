4e20a6db0245f952eb150fee525653c13726e8aa
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-18 19:03:01 +0600

    Not Found Screen

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -9,16 +9,16 @@ export default function NotFoundScreen() {
     return (
         <View style={tw`flex-1 items-center justify-center bg-white px-6`}>
             <Ionicons name="alert-circle-outline" size={64} color="#888" />
-            <Text style={tw`text-2xl font-bold text-gray-800 mt-4`}>Page Not Found</Text>
-            <Text style={tw`text-base text-gray-600 mt-2 text-center`}>
+            <Text style={tw`text-2xl font-bold text-blackish mt-4`}>Page Not Found</Text>
+            <Text style={tw`text-base  text-blackish-600 mt-2 text-center`}>
                 The page you're looking for doesn't exist or has been moved.
             </Text>
 
             <TouchableOpacity
                 onPress={() => router.replace("/")}
-                style={tw`mt-6 bg-blue-500 px-6 py-3 rounded-xl`}
+                style={tw`mt-6 bg-yellowGreen px-6 py-3 rounded-xl`}
             >
-                <Text style={tw`text-white font-semibold`}>Go Home</Text>
+                <Text style={tw`text-blackish font-semibold`}>Go Home</Text>
             </TouchableOpacity>
         </View>
     );
