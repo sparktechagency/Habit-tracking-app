d3a09cbde4522171d743b06cf60dd8dc57f6f41f
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-18 19:20:28 +0600

    Register Screen

---
+++ src/app/(auth)/register.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function RegisterScreen() {
+    return (
+        <View>
+            <Text>RegisterScreen</Text>
+        </View>
+    )
+}
