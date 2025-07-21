98a0d06e9879d5b1e50ec12fd552e5973cf3718c
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-21 20:19:35 +0600

    updated

--- src/app/(tab)/say-on.tsx
+++ src/app/(tab)/say-on.tsx
@@ -1,9 +1,10 @@
+import tw from '@/src/lib/tailwind'
 import React from 'react'
 import { Text, View } from 'react-native'
 
 export default function SayOnScreen() {
     return (
-        <View>
+        <View style={tw`bg-red-200 flex-1`}>
             <Text>SayOnScreen</Text>
         </View>
     )
