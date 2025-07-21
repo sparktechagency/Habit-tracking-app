362ec28a139e59ba99284a4d23e6f5bda47acfb8
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-21 20:15:16 +0600

    added the status bar

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,33 +1,37 @@
 import { Stack } from "expo-router";
+import { StatusBar, View } from "react-native";
 
 export default function AuthRootLayout() {
 
     return (
-        <Stack
-            screenOptions={{
-                headerShown: false,
-            }}
-        >
-            <Stack.Screen name="index" options={{
-                animation: 'slide_from_right'
-            }} />
-            <Stack.Screen name="loging" options={{
-                animation: 'slide_from_right'
-            }} />
-            <Stack.Screen name="register" options={{
-                animation: 'slide_from_right'
-            }} />
-            <Stack.Screen name="verification-opt" options={{
-                animation: 'slide_from_right'
-            }} />
-            <Stack.Screen name="created-new-password" options={{
-                animation: 'slide_from_right'
-            }} />
-            <Stack.Screen name="verify-email" options={{
-                animation: 'slide_from_right'
-            }} />
+        <View style={{ flex: 1 }}>
+            <StatusBar barStyle={'light-content'} />
 
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
+                <Stack.Screen name="index" options={{
+                    animation: 'slide_from_right'
+                }} />
+                <Stack.Screen name="loging" options={{
+                    animation: 'slide_from_right'
+                }} />
+                <Stack.Screen name="register" options={{
+                    animation: 'slide_from_right'
+                }} />
+                <Stack.Screen name="verification-opt" options={{
+                    animation: 'slide_from_right'
+                }} />
+                <Stack.Screen name="created-new-password" options={{
+                    animation: 'slide_from_right'
+                }} />
+                <Stack.Screen name="verify-email" options={{
+                    animation: 'slide_from_right'
+                }} />
 
-        </Stack>
+            </Stack>
+        </View>
     );
 }
