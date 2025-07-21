e3122ea55a11289261b0efb67c1cc5b0834b3fd8
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-21 20:29:24 +0600

    Overall Progress Card components

---
+++ src/components/ui/OverallProgress.tsx
@@ -0,0 +1,187 @@
+import { IconArrowTop, IconCup, IconDate, IconGroup, IconRedArrow, IconsGrowth } from '@/assets/icons';
+import tw from '@/src/lib/tailwind';
+import React from 'react';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SvgXml } from 'react-native-svg';
+import DailyCompletionRatesChart from './DailyCompletionRatesChart';
+import ProgressBar from './ProgressBar';
+
+const OverallProgressCard = () => {
+    return (
+        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{
+            paddingHorizontal: 25,
+            paddingBottom: 8,
+            // flex: 1
+        }} >
+            <View style={tw`bg-primaryBg rounded-xl p-5 shadow-lg shadow-blackish my-2 `}>
+                {/* Header Row */}
+                <View style={tw`flex-col justify-between  gap-1 `}>
+                    <View style={tw`flex-row justify-between items-center `}>
+                        <Text style={tw`text-blackish font-montserrat-500 text-sm`}>
+                            Overall Progress
+                        </Text>
+                        <TouchableOpacity
+                            style={tw`bg-gray rounded-md p-2`}
+                            activeOpacity={0.7}
+                        >
+                            <SvgXml xml={IconsGrowth} width={22} height={22} />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        {/* Progress Number */}
+                        <Text style={tw`text-blackish text-xl font-montserrat-700 `}>
+                            44%
+                        </Text>
+                    </View>
+
+                    <View style={tw`flex-col justify-between  gap-1 `}>
+                        <View>
+                            {/* Subtext */}
+                            <Text style={tw`text-gray-700 text-sm font-montserrat-400`}>
+                                Completion rate across all tasks
+                            </Text>
+                        </View>
+
+                        {/* Bottom Hint */}
+                        <View style={tw`flex-row items-center mt-1`}>
+                            <SvgXml xml={IconArrowTop} width={12} height={12} />
+                            <Text style={tw`text-xs text-green-600 ml-1 font-montserrat-400`}>
+                                12% from last week
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            {/*  */}
+            <View style={tw`bg-primaryBg rounded-xl p-5 shadow-lg shadow-blackish my-2 `}>
+                {/* Header Row */}
+                <View style={tw`flex-col justify-between  gap-1 `}>
+                    <View style={tw`flex-row justify-between items-center `}>
+                        <Text style={tw`text-blackish font-montserrat-500 text-sm`}>
+                            Challenge Progress
+                        </Text>
+                        <TouchableOpacity
+                            style={tw`bg-gray rounded-md p-2`}
+                        // activeOpacity={0.7}
+                        >
+                            <SvgXml xml={IconCup} width={22} height={22} />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        {/* Progress Number */}
+                        <Text style={tw`text-blackish text-xl font-montserrat-700 `}>
+                            Day 59
+                        </Text>
+                    </View>
+
+                    <View style={tw`flex-col justify-between  gap-1 `}>
+                        <View>
+                            {/* Subtext */}
+                            <Text style={tw`text-gray-700 text-sm font-montserrat-400`}>
+                                of 100 day challenge
+                            </Text>
+                        </View>
+
+                        {/* Bottom Hint */}
+                        <View style={tw`flex-row items-center mt-1`}>
+                            <SvgXml xml={IconArrowTop} width={12} height={12} />
+                            <Text style={tw`text-xs text-green-600 ml-1 font-montserrat-400`}>
+                                On track
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            {/*  */}
+            <View style={tw`bg-primaryBg rounded-xl p-5 shadow-lg shadow-blackish my-2 `}>
+                {/* Header Row */}
+                <View style={tw`flex-col justify-between  gap-1 `}>
+                    <View style={tw`flex-row justify-between items-center `}>
+                        <Text style={tw`text-blackish font-montserrat-500 text-sm`}>
+                            Group Completion
+                        </Text>
+                        <TouchableOpacity
+                            style={tw`bg-gray rounded-md p-2`}
+                            activeOpacity={0.7}
+                        >
+                            <SvgXml xml={IconGroup} width={22} height={22} />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        {/* Progress Number */}
+                        <Text style={tw`text-blackish text-xl font-montserrat-700 `}>
+                            44%
+                        </Text>
+                    </View>
+
+                    <View style={tw`flex-col justify-between  gap-1 `}>
+                        <View>
+                            {/* Subtext */}
+                            <Text style={tw`text-gray-700 text-sm font-montserrat-400`}>
+                                of 100 day challenge
+                            </Text>
+                        </View>
+
+                        {/* Bottom Hint */}
+                        <View style={tw`flex-row items-center mt-1`}>
+                            <SvgXml xml={IconRedArrow} width={12} height={12} />
+                            <Text style={tw`text-xs text-[#ff0800] ml-1 font-montserrat-400`}>
+                                5% from last week
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            {/*  */}
+            <View style={tw`bg-primaryBg rounded-xl p-5 shadow-lg shadow-blackish my-2 `}>
+                {/* Header Row */}
+                <View style={tw`flex-col justify-between  gap-3 `}>
+                    <View style={tw`flex-row justify-between items-center `}>
+                        <Text style={tw`text-blackish font-montserrat-500 text-sm`}>
+                            Overall Progress
+                        </Text>
+
+                    </View>
+
+                    <View style={tw`flex-col gap-4`}>
+                        <View style={tw`flex-col gap-2`}>
+                            <View style={tw`flex-row  items-center justify-between`}>
+                                <Text style={tw`text-blackish font-montserrat-700 text-sm  `}>Completion Rate</Text>
+                                <Text style={tw`text-blackish font-montserrat-700 text-sm `}>70%</Text>
+                            </View>
+                            <ProgressBar height={6} progress={7 * 10} color={'#D6DF22'} />
+                        </View>
+
+                    </View>
+
+                    <View style={tw`flex-col justify-between  gap-1 `}>
+                        <View>
+                            {/* Subtext */}
+                            <Text style={tw`text-gray-700 text-sm font-montserrat-400`}>
+                                of 100 day challenge
+                            </Text>
+                        </View>
+
+                        {/* Bottom Hint */}
+                        <View style={tw`flex-row items-center mt-1`}>
+                            <SvgXml xml={IconDate} width={14} height={14} />
+                            <Text style={tw`text-xs text-blackish ml-1 font-montserrat-400`}>
+                                5% from last week
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+
+
+            {/* graph */}
+            <DailyCompletionRatesChart />
+
+        </ScrollView>
+    );
+};
+
+export default OverallProgressCard;
