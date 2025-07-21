4ca85458b6309ca858107d5243a8441325e03fcd
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-21 20:13:53 +0600

    all type

--- src/constants/type.ts
+++ src/constants/type.ts
@@ -2,3 +2,17 @@ export type Tab = {
   key: string;
   label: string;
 };
+
+export type ProgressProps = {
+  color: any;
+  progress: number;
+  height?: number;
+};
+export type AddGroupProps = {
+  title: string;
+  addHeading?: string;
+};
+export type JoinChallengeModalProps = {
+  modalVisible: boolean;
+  setModalVisible: (visible: boolean) => void;
+};
