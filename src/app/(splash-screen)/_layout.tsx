659de68dddaa6ab6caab7dad832f54fd78675fc8
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-21 20:17:14 +0600

    added the statusbar react native

--- src/app/(splash-screen)/_layout.tsx
+++ src/app/(splash-screen)/_layout.tsx
@@ -1,18 +1,24 @@
 import { Stack } from "expo-router";
+import { StatusBar, View } from "react-native";
 
 export default function SplashScreenRootLayout() {
 
     return (
-        <Stack
-            screenOptions={{
-                headerShown: false,
-            }}
-        >
-            <Stack.Screen name="index" options={{}} />
-            <Stack.Screen name="splash-screen-one" options={{ animation: 'slide_from_right' }} />
-            <Stack.Screen name="splash-screen-tow" options={{ animation: 'slide_from_right' }} />
-            <Stack.Screen name="splash-screen-three" options={{ animation: 'slide_from_right' }} />
-            {/* Add other screens here */}
-        </Stack>
+        <View style={{ flex: 1 }}>
+            <StatusBar barStyle={'light-content'} />
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                }}
+
+
+            >
+                <Stack.Screen name="index" options={{}} />
+                <Stack.Screen name="splash-screen-one" options={{ animation: 'slide_from_right' }} />
+                <Stack.Screen name="splash-screen-tow" options={{ animation: 'slide_from_right' }} />
+                <Stack.Screen name="splash-screen-three" options={{ animation: 'slide_from_right' }} />
+                {/* Add other screens here */}
+            </Stack>
+        </View>
     );
 }
