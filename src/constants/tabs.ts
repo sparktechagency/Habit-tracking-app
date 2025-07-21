3e71bb928be119a0625720eb115783e642c074ef
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-21 20:13:40 +0600

    tabs contant

--- src/constants/tabs.ts
+++ src/constants/tabs.ts
@@ -4,3 +4,12 @@ export const TABS: Tab[] = [
   { key: "all", label: "All Habits" },
   { key: "my", label: "My Habits" },
 ];
+export const GROUPTABS: Tab[] = [
+  { key: "all", label: "Active Challenges" },
+  { key: "my", label: "Completed Challenges" },
+];
+//
+export const ViewTABS: Tab[] = [
+  { key: "all", label: "Daily Summaries" },
+  { key: "my", label: "Overall Progress" },
+];
