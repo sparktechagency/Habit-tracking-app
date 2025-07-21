2b96cb3cf0a5d745a5727064029c5c9c0cd1dda1
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-21 20:27:59 +0600

    prograss bar components daynamic

--- src/components/ui/ProgressBar.tsx
+++ src/components/ui/ProgressBar.tsx
@@ -1,14 +1,19 @@
+import { ProgressProps } from '@/src/constants/type';
+import tw from '@/src/lib/tailwind';
 import React from 'react';
 import { View } from 'react-native';
-import tw from 'twrnc';
 
-const ProgressBar = ({ progress = 60 }) => {
+const ProgressBar = ({ progress, color, height }: ProgressProps) => {
     return (
-        <View style={tw`h-2.5 w-full bg-gray-100 rounded-full overflow-hidden`}>
+        <View style={tw` w-full bg-[#f2f2f2] rounded-full overflow-hidden`}>
             <View
                 style={[
-                    tw`h-full bg-[#d6df22] rounded-full`,
-                    { width: `${progress}%` }
+                    tw`h-full rounded-full`,
+                    {
+                        width: `${progress}%`,
+                        backgroundColor: color, // ✅ use color directly
+                        height: height
+                    },
                 ]}
             />
         </View>
