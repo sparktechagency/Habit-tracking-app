134b75aed8f0c014603531e25caf8e2b5ad3d38b
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-23 19:10:36 +0600

    Profile Back Button

---
+++ src/components/ui/ProfileBackButton.tsx
@@ -0,0 +1,22 @@
+import { IconsProfileBack } from '@/assets/icons'
+import { BackProfileProps } from '@/src/constants/type'
+import tw from '@/src/lib/tailwind'
+import { router } from 'expo-router'
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { SvgXml } from 'react-native-svg'
+
+
+
+const ProfileBackButton = ({ title }: BackProfileProps) => {
+    return (
+        <View style={tw`pt-4 pb-7 px-4`}>
+            <TouchableOpacity style={tw` flex-row gap-2.5`} onPress={() => router.back()} >
+                <SvgXml xml={IconsProfileBack} />
+                <Text style={tw`text-xl font-montserrat-700 text-blackish `} >{title}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default ProfileBackButton
