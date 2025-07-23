3a92736a932440b0338e1dfbdb1a78c750084c8f
TANVIR0222 <tanvirislam3912@gmail.com>
2025-07-23 18:57:30 +0600

    get route

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,9 +1,12 @@
-import tw from '@/src/lib/tailwind'
-import React from 'react'
-import { View } from 'react-native'
-import LoginScreen from './loging'
+import tw from '@/src/lib/tailwind';
+import { storage } from '@/src/utils/localStorage';
+import React from 'react';
+import { View } from 'react-native';
+import LoginScreen from './loging';
 
 export default function index() {
+    const role = storage.getString('role');
+    // console.log('Current role:', role);
     return (
         <View style={tw`flex-1`}>
             <LoginScreen />
